19c28b3f96382c357ab4583383a7d8b1305bf6c8
Pyeonseohee <pshsh9234@bme.gachon.ac.kr>
2023-05-27 10:03:24 +0900

    init tcpsocket

---
+++ Test2/Client1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // EC2 인스턴스의 IP 주소와 포트 번호
+            string ipAddress = "52.206.228.119";
+            int port = 50000;
+            Console.WriteLine("시작합니다!");
+            try
+            {
+                // 서버에 연결
+                TcpClient client = new TcpClient(ipAddress, port);
+                Console.WriteLine("서버에 연결되었습니다.");
+
+                // 서버와 데이터 교환
+                NetworkStream stream = client.GetStream();
+
+                // 데이터를 전송할 버퍼
+                byte[] buffer = Encoding.ASCII.GetBytes("안녕하세요, EC2 서버!");
+
+                // 데이터 전송
+                stream.Write(buffer, 0, buffer.Length);
+                Console.WriteLine("데이터를 서버로 전송했습니다.");
+
+                //// 서버로부터 응답을 받음
+                //buffer = new byte[1024];
+                //int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                //string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                //Console.WriteLine("서버 응답: " + response);
+
+                // 연결 종료
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("오류: " + e.Message);
+            }
+
+            Console.ReadLine();
+        }
+        }
+}
---
+++ Test2/Server1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // 서버의 IP 주소와 포트 번호
+            string ipAddress = "52.206.228.119";
+            int port = 50000;
+
+            try
+            {
+                // 서버 소켓 생성
+                TcpListener server = new TcpListener(IPAddress.Parse(ipAddress), port);
+
+                // 클라이언트 연결 대기
+                server.Start();
+                Console.WriteLine("서버가 시작되었습니다. 클라이언트 연결 대기 중...");
+
+                // 클라이언트 연결 수락
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("클라이언트가 연결되었습니다.");
+
+                // 클라이언트와 데이터 교환
+                NetworkStream stream = client.GetStream();
+
+                //// 클라이언트로부터 데이터 수신
+                //byte[] buffer = new byte[1024];
+                //int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                //string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                //Console.WriteLine("클라이언트로부터 데이터를 수신했습니다: " + dataReceived);
+
+                //// 클라이언트에 응답 전송
+                //string response = "서버에서 클라이언트로 응답합니다.";
+                //byte[] responseData = Encoding.ASCII.GetBytes(response);
+                //stream.Write(responseData, 0, responseData.Length);
+                //Console.WriteLine("클라이언트에 응답을 전송했습니다.");
+
+                // 연결 종료
+                client.Close();
+                server.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("오류: " + e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
