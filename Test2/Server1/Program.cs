fd1d6e7a2c194c09611ab43a5aef806dac3b4cb9
Pyeonseohee <voiceinter@naver.com>
2023-05-29 10:52:07 +0900

    add multi-thread(but not test)

--- Test2/Server1/Program.cs
+++ Test2/Server1/Program.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
 
 namespace Server1
 {
@@ -22,28 +24,60 @@ namespace Server1
                 server.Start();
                 Console.WriteLine("서버가 시작되었습니다. 클라이언트 연결 대기 중...");
 
+                while (true)
+                {
+                    // 클라이언트 연결 수락
+                    TcpClient AcceptedClient = server.AcceptTcpClient();
+
+                    // 새로운 클라이언트가 연결되면 쓰레드 생성 및 시작
+                    Thread clientThread = new Thread(HandleClient);
+                    clientThread.Start(AcceptedClient);
+                }
+
                 // 클라이언트 연결 수락
                 TcpClient client = server.AcceptTcpClient();
                 Console.WriteLine("클라이언트가 연결되었습니다.");
 
-                // 클라이언트와 데이터 교환
-                NetworkStream stream = client.GetStream();
 
-                //// 클라이언트로부터 데이터 수신
-                //byte[] buffer = new byte[1024];
-                //int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                //string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                //Console.WriteLine("클라이언트로부터 데이터를 수신했습니다: " + dataReceived);
+                void HandleClient(object clientObj)
+                {
+                    TcpClient client = (TcpClient)clientObj;
+                    Console.WriteLine("클라이언트 연결됨: " + client.Client.RemoteEndPoint);
 
-                //// 클라이언트에 응답 전송
-                //string response = "서버에서 클라이언트로 응답합니다.";
-                //byte[] responseData = Encoding.ASCII.GetBytes(response);
-                //stream.Write(responseData, 0, responseData.Length);
-                //Console.WriteLine("클라이언트에 응답을 전송했습니다.");
+                    // 클라이언트와의 데이터 통신 처리
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
 
-                // 연결 종료
-                client.Close();
-                server.Stop();
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        // 수신한 데이터 처리
+                        string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        var jsonObject = JsonConvert.DeserializeObject<dynamic>(data);
+                        Console.WriteLine(jsonObject);
+                        if (jsonObject.Route == "Login")
+                        {
+                            LoginHandler();
+                        }
+                        else if (jsonObject.Route == "Register")
+                        {
+                            RegisterHandler();
+                        }
+                        else
+                        {
+                            Console.WriteLine("else");
+                        }
+                        // 클라이언트에게 응답 전송
+                        byte[] response = Encoding.UTF8.GetBytes("서버 응답: " + data);
+                        stream.Write(response, 0, response.Length);
+                    }
+
+                    // 클라이언트와의 연결 종료
+                    Console.WriteLine("클라이언트 연결 종료: " + client.Client.RemoteEndPoint);
+                    client.Close();
+                }
+
             }
             catch (Exception e)
             {
@@ -52,5 +86,18 @@ namespace Server1
 
             Console.ReadLine();
         }
+        // Login packet
+        public static void LoginHandler()
+        {
+            Console.WriteLine("Login Handler!");
+        }
+
+        // Register packet
+        public static void RegisterHandler()
+        {
+            Console.WriteLine("Register Handler!");
+        }
+
+
     }
 }
